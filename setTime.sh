1b67e843b656476f695e7737b97385e4a87374bd
davidswygart <davidswygart@gmail.com>
2024-10-15 17:51:45 -0400

    Update setTime.sh

--- setTime.sh
+++ setTime.sh
@@ -2,7 +2,4 @@
 day="$(date +%Y-%m-%d)";
 time="$(date +%H:%M:%S)";
 echo setting time on all Pis to $day $time
-#command="sudo sh pins/setTime.sh $now"
-#echo $command
-#cssh piCluster -a $command
 cssh piCluster -a "sudo sh MAVRS_pi/setTime.sh $day $time"
