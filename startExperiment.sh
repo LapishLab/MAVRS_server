e310edd7aecf72211b45e91145e68a7302bf8c8a
ephys_rig_A <davidswygart@gmail.com>
2025-04-14 11:52:01 -0400

    working with static IP

--- startExperiment.sh
+++ startExperiment.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 cd $(dirname "$(readlink -f "$0")")
 
-day="$(date +%F)_"
+prefix="$(date +%Y-%m-%d_%H-%M-%S)_rat"
 
 echo 'Enter session name:'
-read -e -i "$day" session
+read -e -i "$prefix" session
+
+echo 'creating MED-PC folder'
+ssh dlinsenb@10.0.0.3 'mkdir \data\'$session'\med-pc_'$session
+
+echo 'creating experiment folder on this PC'
+mkdir "/mnt/c/data/$session"
+
+echo 'creating Anymaze folder'
+mkdir "/mnt/c/data/$session/anymaze_$session"
+
+echo 'creating open-ephys folder'
+mkdir "/mnt/c/data/$session/open-ephys_$session"
+
+echo 'Hit enter when ready to start Pi recording'
+read
 
 echo "experiment started at $(date)"
-cssh piCluster -a "python -u MAVRS_pi/startExperiment.py --session $session"
+ssh pi@10.1.1.2 "python -u MAVRS_pi/startExperiment.py --session $session/pi-data_$session"
+#cssh piCluster -a "python -u MAVRS_pi/startExperiment.py --session $session/pi-data_$session"
--- transferData.sh
+++ transferData.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-cssh piCluster -a "bash MAVRS_pi/transferData.sh server /home/server/Desktop/piData"
+#cssh piCluster -a "bash MAVRS_pi/transferData.sh server /home/server/Desktop/piData"
+
+echo "copying data from  MED-PC"
+rsync --progress -a 'dlinsenb@10.0.0.3:/data/' '/mnt/c/data/'
+
+echo "copying data from Pi"
+rsync --progress -a 'pi@10.1.1.2:~/MAVRS_pi/data/' '/mnt/c/data/'
