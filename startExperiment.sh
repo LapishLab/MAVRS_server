1f2342a8fa06441dd84ff0c42569aa5107d4841c
David Swygart <davidswygart@gmail.com>
2025-04-21 17:29:56 -0400

    placeholder for medPC folder creation

--- startExperiment.sh
+++ startExperiment.sh
@@ -11,8 +11,14 @@ time="$(date +%Y-%m-%d_%H-%M-%S)"
 
 suggested="${time}_${exp}_group${group}"
 
-echo 'Verify correct name, then hit enter to start recording:'
+echo 'Verify correct name, then hit enter'
 read -e -i "$suggested" session
+
+echo 'creating MED-PC folder'
+#ssh lapishla@10.0.0.1 'mkdir \data\'$session'\med-pc_'$session
+
+echo 'Hit enter when ready to start Pi recording'
+read
 
 echo "started recording ${session}"
 cssh piCluster -a "python -u MAVRS_pi/startExperiment.py --session ${session}"
