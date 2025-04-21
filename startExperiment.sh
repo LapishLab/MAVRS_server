06511901993f2e4a311c589711d5703d44b82e97
David Swygart <davidswygart@gmail.com>
2025-04-21 15:21:40 -0400

    update naming convention

--- startExperiment.sh
+++ startExperiment.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 cd $(dirname "$(readlink -f "$0")")
 
-day="$(date +%F)_"
+echo 'Enter experiment name:'
+read -e exp
 
-echo 'Enter session name:'
-read -e -i "$day" session
+echo 'Enter group number:'
+read -e group
 
-echo "experiment started at $(date)"
-cssh piCluster -a "python -u MAVRS_pi/startExperiment.py --session $session"
+time="$(date +%Y-%m-%d_%H-%M-%S)"
+
+suggested="${time}_${exp}_group${group}"
+
+echo 'Verify correct name, then hit enter to start recording:'
+read -e -i "$suggested" session
+
+echo "started recording ${session}"
+cssh piCluster -a "python -u MAVRS_pi/startExperiment.py --session ${session}"
