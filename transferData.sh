2cfb3d215abd05f14ef333343b8539e645d31fb2
David Swygart <davidswygart@gmail.com>
2025-04-23 16:23:14 -0400

    copy data from MedPC

--- transferData.sh
+++ transferData.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+echo "copying data from  MED-PC"
+rsync --progress -a 'lapishla@10.1.1.2:/mnt/c/data/' '/home/lapishla/Desktop/piData'
+echo "MED-PC Sync completed: Hit enter to begin pi transfer"
+read
+
+echo "copying data from Pi"
 cssh piCluster -a "bash MAVRS_pi/transferData.sh lapishla /home/lapishla/Desktop/piData"
