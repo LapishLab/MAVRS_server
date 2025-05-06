b52df6341375435b2858ea8423a14d787e49d677
David Swygart <davidswygart@gmail.com>
2025-05-06 13:47:44 -0400

    better rsync messeging

--- backup_data.sh
+++ backup_data.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 echo "Uploading the piData folder to DataStar"
-rsync -a --progress "/home/lapishla/Desktop/piData/" "lapishla@datastar.psych.indianapolis.iu.edu:/research/behavior_rooms/2CAP/"
-echo "Transfer complete: Hit enter to close window"
-read
+rsync -ah --info=progress2 "/home/lapishla/Desktop/piData/" "lapishla@datastar.psych.indianapolis.iu.edu:/research/behavior_rooms/2CAP/"
+
+if [ $? -eq 0 ]; then
+    echo
+    echo "---Transfer Complete---"
+    echo "Data has been backed up to Datastar"
+    echo "-----------------------"
+    echo
+else
+    echo
+    echo "---WARNING---"
+    echo "Rsyncing to Datastar failed!"
+    echo "-------------"
+    echo
+fi
+
+read -p "Hit enter to close window"
