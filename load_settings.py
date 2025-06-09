dc9a37574efbd09a17c1ecae74e64161e30fa818
david swygart (DD) <davidswygart@gmail.com>
2025-06-09 12:39:56 -0400

    store config files in home/.config/MAVRS_server

--- load_settings.py
+++ load_settings.py
@@ -1,16 +1,27 @@
 from yaml import full_load
-from os.path import dirname, realpath
+# from os.path import dirname, realpath, join
+from pathlib import Path
 
 def load_settings():
-    script_dir = dirname(realpath(__file__))
-    settings_file = script_dir + "/settings.yaml"
+    settings_file = Path.home() / ".config/MAVRS_server/settings.yaml"
     with open(settings_file, 'r') as f:
             settings = full_load(f)
     return settings
 
 def load_experiment_names():
-    script_dir = dirname(realpath(__file__))
-    names_file = script_dir + "/experiment_names.txt"
+    names_file = Path.home() / ".config/MAVRS_server/experiment_names.txt"
     with open(names_file, "r") as file:
             lines = file.readlines()
     return lines
+
+def test():
+        settings = load_settings()
+        print("Settings loaded successfully.")
+        print(settings)
+        
+        experiment_names = load_experiment_names()
+        print("Experiment names loaded successfully.")
+        print(experiment_names)
+
+if __name__ == "__main__":
+    test()
