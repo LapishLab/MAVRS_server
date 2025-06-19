0069b2a86eb899c66ce22042944aeae0d0ec8c13
ephys_rig_A <davidswygart@gmail.com>
2025-06-19 12:40:55 -0400

    start of script to copy config settings

---
+++ setup/config_templates/copy_settings.sh
@@ -0,0 +1,3 @@
+mkdir -p  ~/.config/MAVRS_server
+cp ./setup/config_templates/experiment_names.txt ~/.config/MAVRS_server/experiment_names.txt
+cp ./setup/config_templates/settings.yaml  ~/.config/MAVRS_server/settings.yaml
