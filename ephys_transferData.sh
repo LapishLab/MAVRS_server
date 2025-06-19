7e54bb7af9a06f1d5680df88eee2a9cca0d9180c
Ephys Rig B <davidswygart@gmail.com>
2025-06-19 13:34:31 -0400

    delete ephys_transferData.sh, use python script and settings

--- ephys_transferData.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "copying data from  MED-PC"
-rsync --progress -a 'lapishla@10.0.0.1:/data/' '/mnt/c/data/'
-
-echo "copying data from Pi"
-rsync --progress -a 'pi@10.0.0.2:~/MAVRS_pi/data/' '/mnt/c/data/'
