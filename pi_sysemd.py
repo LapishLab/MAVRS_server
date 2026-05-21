d2491d333619c0bd49925accbbb6dd395b93fca9
david swygart (DD) <davidswygart@gmail.com>
2026-05-21 17:08:51 -0400

    simplify unit name

--- pi_sysemd.py
+++ pi_sysemd.py
@@ -1,5 +1,5 @@
 ENV = "XDG_RUNTIME_DIR=/run/user/$(id -u)"
-UNIT = "MAVRS_exp"
+UNIT = "mavrs"
 SCRIPT_PATH = "/home/pi/MAVRS_pi/startExperiment.py"
 PYTHON_PATH = "/home/pi/MAVRS_pi/.venv/bin/python3"
 
